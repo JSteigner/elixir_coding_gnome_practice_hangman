ee71ee17b5fdf134d1e588be4386e516fa0a1307
Jay Steigner <ioctosteigner@gmail.com>
2021-03-07 10:57:15 -0600

    updated boilerplate to use hangman_controller

--- gallows/mix.exs
+++ gallows/mix.exs
@@ -13,10 +13,6 @@ defmodule Gallows.MixProject do
       deps: deps()
     ]
   end
-
-  # Configuration for the OTP application.
-  #
-  # Type `mix help compile.app` for more information.
   def application do
     [
       mod: {Gallows.Application, []},
@@ -24,13 +20,9 @@ defmodule Gallows.MixProject do
     ]
   end
 
-  # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
-  # Specifies your project dependencies.
-  #
-  # Type `mix help deps` for examples and options.
   defp deps do
     [
       {:phoenix, "~> 1.5.8"},
@@ -41,16 +33,12 @@ defmodule Gallows.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+
+      {:hangman, [ path: "../hangman" ]},
     ]
   end
 
-  # Aliases are shortcuts or tasks specific to the current project.
-  # For example, to install project dependencies and perform other setup tasks, run:
-  #
-  #     $ mix setup
-  #
-  # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
       setup: ["deps.get", "cmd npm install --prefix assets"]
